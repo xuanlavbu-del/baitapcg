b3796eee0b4772b333d2fa72b643d066de10aab2
xuanlavbu-del <xuanla.vbu@gmail.com>
2026-03-19 22:24:08 +0900

    thêm file ex1.html

---
+++ src/ecma.js
@@ -0,0 +1,13 @@
+let answer = prompt('What is the "official" name of JavaScript?');
+
+if (answer === "ECMAScript") {
+    alert("Right!");
+} else {
+    alert("Didn’t know? ECMAScript!");
+}
+let value = prompt('What is the "official" name of JavaScript?', '');
+if (value == 'ECMAScript') {
+    alert('Right!');
+} else {
+    alert("You don't know? ECMAScript!");
+}
